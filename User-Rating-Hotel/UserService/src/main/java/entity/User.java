9090e0792aeb29fdec3a2b01e7ff8dffefb4f107
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-04-14 16:03:23 +0530

    User entity

---
+++ User-Rating-Hotel/UserService/src/main/java/entity/User.java
@@ -0,0 +1,48 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "micro_users")
+public class User {
+
+	@Id
+	@Column(name = "ID")
+	private String userid;
+	@Column(name = "NAME")
+	private String name;
+	@Column(name = "EMAIL")
+	private String email;
+	@Column(name = "ABOUT")
+	private String about;
+	public User() {
+		super();
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+}

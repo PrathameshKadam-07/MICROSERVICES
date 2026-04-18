0609a552b07b0884a1247acf08fcc1d2338ee838
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-04-18 15:41:14 +0530

    Microservices UserServiceController Client RatingUpdate

--- User-Rating-Hotel/UserService/src/main/java/com/controller/Usercontroller.java
+++ User-Rating-Hotel/UserService/src/main/java/com/controller/Usercontroller.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.service.RatingClient;
 import com.service.UserService;
 import com.entity.User;
 
@@ -23,6 +25,9 @@ public class Usercontroller {
 	@Autowired
 	UserService us;
 	
+	@Autowired
+	RatingClient rc;
+	
 	@PostMapping
 	public ResponseEntity<User>  saveUser(@RequestBody User user) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(us.saveUser(user));
@@ -30,12 +35,20 @@ public class Usercontroller {
 	
 	@GetMapping("/getalluser")
 	public ResponseEntity<List<User>> getAllUser() {
-		return ResponseEntity.ok(us.getalluser());
+		List<User> userlist = us.getalluser();
+
+		List<User> userlistup =  userlist.stream().map(user -> {user.setRating(rc.getRatingByUserid(user.getUserid()));
+				return user;}).collect(Collectors.toList());
+		
+		return ResponseEntity.ok(userlistup);
 	}
 	
 	@GetMapping("/getUserByid")
 	public ResponseEntity<User> getUserById(@RequestParam("userId") String userId) {
-		return  ResponseEntity.ok(us.getuserByUserId(userId));
+		User user = us.getuserByUserId(userId);
+		user.setRating(rc.getRatingByUserid(user.getUserid()));
+
+		return  ResponseEntity.ok(user);
 	}
 	
 	@DeleteMapping("/deleteUser")
